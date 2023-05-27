fc44efbfc4b636e7fd8340c6d148199788532ac2
JoaoB <joaoboscocalaisn@gmail.com>
2023-05-27 11:43:26 -0300

    first step

--- src/main/java/br/com/curso/GreetingController.java
+++ src/main/java/br/com/curso/GreetingController.java
@@ -11,8 +11,7 @@ public class GreetingController {
 	
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
-	
-	
+
 	@RequestMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "world") String name){
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
